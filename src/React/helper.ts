0317dbda7723602a7c882705384c6e7f42178063
xuzhen <richxu@dfocuspace.com>
2020-09-29 18:55:26 +0800

    feat: React test

---
+++ src/React/helper.ts
@@ -0,0 +1,28 @@
+import { getLocationHref, getTimestamp } from 'utils'
+import { ERRORTYPES, BREADCRUMBTYPES } from '@/common'
+// import { ViewModel } from './types'
+import { breadcrumb, transportData } from 'core'
+import { ReportDataType } from '@/types/transportData'
+import { Severity } from '@/utils/Severity'
+
+export function handleReactError(err: Error, vm, info: object, level: Severity, breadcrumbLevel: Severity): void {
+  const propsData = info
+  const data: ReportDataType = {
+    type: ERRORTYPES.REACT_ERROR,
+    message: `${err.message}(${info})`,
+    level,
+    url: getLocationHref(),
+    componentName: vm,
+    propsData: propsData || '',
+    name: err.name,
+    stack: err.stack || [],
+    time: getTimestamp()
+  }
+  breadcrumb.push({
+    type: BREADCRUMBTYPES.REACT,
+    category: breadcrumb.getCategory(BREADCRUMBTYPES.REACT),
+    data,
+    level: breadcrumbLevel
+  })
+  transportData.xhrPost(data)
+}
