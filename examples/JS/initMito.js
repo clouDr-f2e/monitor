1aefe54cf7afffdfc2766b51f679fe4d5dd08456
jinhuochen <1443970739@qq.com>
2020-10-10 22:22:18 +0800

    feat:添加js example

---
+++ examples/JS/initMito.js
@@ -0,0 +1,10 @@
+MITO.init({
+  debug: true,
+  dsn: 'http://end/error.gif',
+  // 默认20
+  maxBreadcrumbs: 10
+  // 默认false
+  // enableTraceId: true,
+  // 默认截取所有接口
+  // filterXhrUrlRegExp: /\/likePoetry/
+})
