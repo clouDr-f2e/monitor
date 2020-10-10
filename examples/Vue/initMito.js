55d93731d6ba9b42725c663e2925b7c76b4c19e9
jinhuochen <1443970739@qq.com>
2020-10-10 22:15:48 +0800

    feat：添加vue的example

---
+++ examples/Vue/initMito.js
@@ -0,0 +1,11 @@
+Vue.use(MITO.MitoVue)
+MITO.init({
+  debug: true,
+  dsn: 'http://end/error.gif',
+  // 默认20
+  maxBreadcrumbs: 10
+  // 默认false
+  // enableTraceId: true,
+  // 默认截取所有接口
+  // filterXhrUrlRegExp: /\/likePoetry/
+})
