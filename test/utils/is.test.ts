65c1fe9bb074732da9c92feb8a777f5af686dc51
jinhuochen <1443970739@qq.com>
2020-12-24 12:43:21 +0800

    feat：添加jest.mock测试

---
+++ test/utils/is.test.ts
@@ -0,0 +1,6 @@
+import { isArray, variableTypeDetection } from '../../src/utils/is'
+test('variableTypeDetection', () => {
+  const arr = []
+  expect(variableTypeDetection.isArray(arr)).toBe(true)
+  expect(isArray(arr)).toBe(true)
+})
