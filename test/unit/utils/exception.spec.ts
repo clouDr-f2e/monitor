b37dd56f52edbe7c06dedafad506ad9c1616d7ab
jinhuochen <1443970739@qq.com>
2021-01-05 15:19:47 +0800

    feat:完成exception单测

---
+++ test/unit/utils/exception.spec.ts
@@ -0,0 +1,24 @@
+import { nativeTryCatch } from '@/utils'
+
+describe('exception.ts', () => {
+  it('should nativeTryCatch func work', () => {
+    const errMsg = 'unti'
+    const throwErrorFunc = () => {
+      throw new Error(errMsg)
+    }
+    nativeTryCatch(throwErrorFunc, (err: Error) => {
+      expect(err.message).toBe(errMsg)
+    })
+  })
+  it("should nativeTryCatch's two params not run under normal circumstances", () => {
+    const normalFun = () => {
+      const a = 1
+      a + 1 === 2
+    }
+    let isRun = false
+    nativeTryCatch(normalFun, () => {
+      isRun = true
+    })
+    expect(isRun).toBeFalsy()
+  })
+})
