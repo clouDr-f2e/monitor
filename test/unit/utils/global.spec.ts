3a8efe36958967010caa8269a38c897ccf873e57
jinhuochen <1443970739@qq.com>
2021-01-05 15:40:10 +0800

    feat：完成global单测的编写

---
+++ test/unit/utils/global.spec.ts
@@ -0,0 +1,28 @@
+import { EVENTTYPES } from '@/common/common'
+import browserIndex from '@/index'
+import { getFlag, getGlobal, getGlobalMitoSupport, isBrowserEnv, isNodeEnv, isWxMiniEnv, setFlag } from '@/utils'
+
+describe('global.ts', () => {
+  it('should getGLoabl func work', () => {
+    const _window = getGlobal()
+    expect(_window).toBe(window)
+  })
+  it('should setFlag and getFlag work', () => {
+    expect(getFlag(EVENTTYPES.DOM)).toBeFalsy()
+    setFlag(EVENTTYPES.DOM, true)
+    expect(getFlag(EVENTTYPES.DOM)).toBeTruthy()
+  })
+  it('should getGlobalMitoSupport func work', () => {
+    browserIndex.init({ maxBreadcrumbs: 16 })
+    const __MITO__ = getGlobalMitoSupport()
+    expect(__MITO__).toBeDefined()
+    expect(__MITO__.breadcrumb).toBeDefined()
+  })
+
+  it('should env variable work', () => {
+    expect(isNodeEnv).toBeTruthy()
+    expect(isWxMiniEnv).toBeFalsy()
+    // mock browser env by node
+    expect(isBrowserEnv).toBeTruthy()
+  })
+})
