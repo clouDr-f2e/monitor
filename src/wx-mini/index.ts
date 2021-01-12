ebf159abf23d8b81a3f9db4bdf5fceb2a4b071ca
jinhuochen <1443970739@qq.com>
2021-01-12 15:17:02 +0800

    feat：添加wx 初始化函数

--- src/wx-mini/index.ts
+++ src/wx-mini/index.ts
@@ -2,11 +2,12 @@ import initOptions from '../common/initOpitons'
 import { InitOptions } from '../types/options'
 import { isWxMiniEnv } from '../utils'
 import { setupReplace } from './load'
-
+import { log } from '../core/index'
+import { SDK_VERSION, SDK_NAME } from '../common/config'
 function init(options: InitOptions = {}) {
   if (!isWxMiniEnv) return
   initOptions(options)
   setupReplace()
 }
 
-export default { init }
+export default { init, log, SDK_VERSION, SDK_NAME }
--- src/wx-mini/utils.ts
+++ src/wx-mini/utils.ts
@@ -1,9 +1,13 @@
+import { variableTypeDetection } from '@/utils'
+
 /**
  * 获取wx当前route的方法
  * 必须是在进入Page或Component构造函数内部才能够获取到currentPages
  * 否则都是在注册Page和Component时执行的代码，此时url默认返回'App'
  */
 export function getCurrentRoute() {
-  const pages = getCurrentPages() // 在App里调用该方法，页面还没有生成，长度为0
-  return pages.length ? getCurrentPages().pop().route : 'App'
+  if (variableTypeDetection.isFunction(getCurrentPages)) {
+    const pages = getCurrentPages() // 在App里调用该方法，页面还没有生成，长度为0
+    return pages.length ? getCurrentPages().pop().route : 'App'
+  }
 }
