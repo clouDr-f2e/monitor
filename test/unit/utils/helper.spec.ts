a0816cdd4112ea23bbfb3665bd27f4af2ef70f16
jinhuochen <1443970739@qq.com>
2021-02-03 10:32:27 +0800

    fix：更改unknownToString单测

--- test/unit/utils/helper.spec.ts
+++ test/unit/utils/helper.spec.ts
@@ -125,8 +125,8 @@ describe('helper.ts', () => {
     expect(unknownToString(test1)).toBe(test1)
     expect(unknownToString(test2)).toBe(JSON.stringify(test2))
     expect(unknownToString(test3)).toBe(JSON.stringify(test3))
-    expect(unknownToString(undefined)).toBe(JSON.stringify('undefined'))
-    expect(unknownToString(null)).toBe(JSON.stringify('null'))
+    expect(unknownToString(undefined)).toBe('undefined')
+    expect(unknownToString(null)).toBe('null')
   })
 
   it('should getBigVersion func work', () => {
