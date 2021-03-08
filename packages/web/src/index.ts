4bb371576158a3b9b555da898a8f88f99799ad4c
jinhuochen <1443970739@qq.com>
2021-03-08 10:46:09 +0800

    feat:company gitignore

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -2,4 +2,3 @@ import { init, SDK_VERSION, SDK_NAME, log } from '@mitojs/browser'
 import { MitoVue } from '@mitojs/vue'
 import { errorBoundaryReport } from '@mitojs/react'
 export { init, SDK_VERSION, SDK_NAME, MitoVue, log, errorBoundaryReport }
-// todo 将声明文件整合到一个.d.ts中，或者将@mito的路径替换下
