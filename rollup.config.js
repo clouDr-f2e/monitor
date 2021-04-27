a34c85eec27d82dbe7585ce16b2d1739d4008fc3
jinhuochen <1443970739@qq.com>
2021-04-27 17:52:33 +0800

    feat:remove rollup-plugin-json

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
+import json from '@rollup/plugin-json'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
 import clear from 'rollup-plugin-clear'
-import json from 'rollup-plugin-json'
 import cleanup from 'rollup-plugin-cleanup'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
