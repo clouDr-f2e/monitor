621d200096aff03e249c94a8c532a166f77359ea
zty1205 <zty1160051490@163.com>
2021-07-19 14:44:37 +0800

    feat: add mini-performance e2e

--- examples/MiniPerformance/app.js
+++ examples/MiniPerformance/app.js
@@ -1,14 +1,19 @@
 const MITO = require('./utils/wx-mini-performance.js')
 
+wx["__MITO_MINI_PERFORMANCE__"] = {
+  data: []
+}
+
 const wxPerformance = new MITO.WxPerformance({
   appId: "a1329cc0-563b-11eb-98fe-259847d73cdd",
   immediately: true,
   report: (data) => {
-    console.log('WxPerformance data = ', data)
+    // console.log('WxPerformance data = ', data)
+    console.log('data = ', data)
+    wx["__MITO_MINI_PERFORMANCE__"].data.push(data)
   }
 })
 
-console.log('wxPerformance = ', wxPerformance)
 
 App({
   onLaunch() {},
